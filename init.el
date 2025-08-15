34fddbfb4a31c4efab34c1b2717b5e362ffec8cf
Roberto Velasco Segura <rvelseg@ciencias.unam.mx>
2025-08-15 12:48:11 -0600

    limpieza

--- init.el
+++ init.el
@@ -365,16 +365,6 @@
 (use-package treesit-fold
   :ensure t)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
+;; Custom file
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
