618e4f21fcdae156aa62dfd77dac807fb5d66e8a
Roberto Velasco Segura <rvelseg@ciencias.unam.mx>
2026-03-29 16:55:06 -0600

    feat: Configure Dired to hide dotfiles by default

--- init.el
+++ init.el
@@ -324,8 +324,7 @@
 	      (" " . jules-dired-open-in-other-window-no-focus)
 	      ("i" . dired-subtree-toggle))
   :config
-  (setq dired-omit-files "^\\...+$")
-  ;(setq dired-omit-files (concat dired-omit-files "\\|^\\..*\\|^_.*")
+  (setq dired-omit-files "^\\..*")
   )
 
 ;;; ChatGPT Shell - OpenAI Integration
